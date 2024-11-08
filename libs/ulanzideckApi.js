2e875968312081cda013dfaba98026200c60dbb1
yu3991 <1038856837@qq.com>
2024-11-08 15:37:26 +0800

    修复setGifPathIcon type值错误

--- libs/ulanzideckApi.js
+++ libs/ulanzideckApi.js
@@ -395,7 +395,7 @@ class UlanzideckApi extends EventEmitter {
           uuid,
           key,
           actionid,
-          type: 3,
+          type: 4,
           gifpath,
           textData: text || '',
           showtext: text ? true : false
